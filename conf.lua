c90fca0b7e58ffba28726cb3c0d9fa2322cb53a7
Karol Bąk <bak.karolek@gmail.com>
2019-10-30 15:34:18 +0100

    Add gif animation and makefile

--- conf.lua
+++ conf.lua
@@ -6,7 +6,8 @@ function love.conf ( t )
     t.modules.physics = false
     t.modules.joystick = false
     t.window.title = "Double pendulum"
-    t.window.fullscreen = true
+    t.window.fullscreen = false
+    t.window.resizable = true
     t.window.vsync = 1
 
 end
